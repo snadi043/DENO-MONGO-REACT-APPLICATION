81fecb83b6fe089f2a43f8a8e323e1633c09d058
snadi043 <snadi043@uottawa.ca>
2025-06-13 12:37:23 -0400

    Integrating the application with OAK framework which is supported by DENO and configuring the application to run the server on the host.

--- app.ts
+++ app.ts
@@ -8,10 +8,25 @@
 //     console.log('file created and updated with text.');
 // });
 
-const body: string = 'Hello World!!!'
+// Importing the "OAK" framework package with the DENO supported URL imports into the application.
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-const server = Deno.serve({port: 8000, hostname: "127.0.0.1"},  (_req) => new Response(body));
+ // The application class manages the HTTP server, handles the middleware functions and deals with the errors 
+const app = new Application();
 
-server.finished.then(() =>{
-    console.log('server created!')
-});
+const body: string = 'Good Morning DEV!'
+
+// middleware in the OAK framework which is similar to node middleware but in OAK a context is provided on every middleware
+// which handles the responses of that middleware and passes to the next middleware compiled as a stack.
+app.use(async (ctx, next) => {
+    await next();
+    ctx.response.body = body;
+})
+
+await app.listen({port: 8000});
+
+// const server = Deno.serve({hostname: "127.0.0.1"},  (_req) => new Response(body));
+
+// server.finished.then(() =>{
+//     console.log('server created!')
+// });
